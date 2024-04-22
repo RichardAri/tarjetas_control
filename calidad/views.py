94b16aa5f3ba5481ac1000736fc0cc2682eac30e
RichardAri <richardaripari@gmail.com>
2024-04-22 12:28:44 -0500

    Se añadio verificacion de datos vacios en el form calidad

--- calidad/views.py
+++ calidad/views.py
@@ -18,31 +18,20 @@ def index(request):
 @login_required
 def create_recurso(request):
     if request.method == 'POST':
-        form = CalidadForm(request.POST or None)
-        if form.is_valid():
-            recurso = form.save()
-            context = {'contact': recurso}
-            return render(request, 'partials/recurso.html', context)
-
-    return render(request, 'partials/form.html', {'form': CalidadForm })
-
-@login_required
-def create_operativo(request):
-    if request.method == 'POST':
         nombre = request.POST.get('nombre', '').strip()
-        equipos = request.POST.get('operativo_equipos', '').strip()
-        materiales = request.POST.get('operativo_materiales', '').strip()
-        herramientas = request.POST.get('operativo_herramientas', '').strip()
-        manoDeObra = request.POST.get('operativo_manodeobra', '').strip()
-        epps = request.POST.get('operativo_epps', '').strip()
-        generales = request.POST.get('operativo_generales', '').strip()
+        peligro = request.POST.get('calidad_peligro', '').strip()
+        riesgo = request.POST.get('calidad_riesgo', '').strip()
+        causas = request.POST.get('calidad_causas', '').strip()
+        control = request.POST.get('calidad_control', '').strip()
 
         # Verificar si todos los campos están llenos
-        if nombre and equipos and materiales and herramientas and manoDeObra and epps and generales:
-            # Si todos los campos están llenos, crear y guardar el objeto RecursoOperativo
-            form = OperativoForm(request.POST)
-            operativo = form.save()
-            context = {'operativo': operativo }
-            #return render(request, 'partials/operativos.html', context)
-            return render(request, 'partials/form_operativos.html', {'form': OperativoForm()})  
-    return render(request, 'partials/form_operativos.html', {'form': OperativoForm()}) 
+        if nombre and peligro and riesgo and causas and control: # Si todos los campos están llenos, crear y guardar el objeto RecursoOperativo
+            form = CalidadForm(request.POST)
+            recurso = form.save()
+            context = {'contact': recurso }
+            return render(request, 'partials/recurso.html', context)
+    return render(request, 'partials/form.html', {'form': CalidadForm })
+
+# !agrega normal los datos pero la pagina /calidad no se recarga 
+# !por lo que el usuario debe de actualizar la pagina 
+# !para ver los datos
